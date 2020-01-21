be302c4e58164cc0be8378ceff8272339e746a7c
RobertoCostaTupinamba <robertotupinamba@gmail.com>
2020-01-21 20:38:03 -0300

    Responsive web

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,90 @@
 import React from 'react';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
 
 function App() {
 
   return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
 
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/41094007?s=460&v=4" alt="Roberto Tupinambá" />
+              <div className="user-info">
+                <strong>Roberto Tupinambá</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Idade: 21 anos</p>
+            <a href="https://github.com/RobertoCostaTupinamba">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/41094007?s=460&v=4" alt="Roberto Tupinambá" />
+              <div className="user-info">
+                <strong>Roberto Tupinambá</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Idade: 21 anos</p>
+            <a href="https://github.com/RobertoCostaTupinamba">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/41094007?s=460&v=4" alt="Roberto Tupinambá" />
+              <div className="user-info">
+                <strong>Roberto Tupinambá</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Idade: 21 anos</p>
+            <a href="https://github.com/RobertoCostaTupinamba">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/41094007?s=460&v=4" alt="Roberto Tupinambá" />
+              <div className="user-info">
+                <strong>Roberto Tupinambá</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Idade: 21 anos</p>
+            <a href="https://github.com/RobertoCostaTupinamba">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
   );
 }
 
